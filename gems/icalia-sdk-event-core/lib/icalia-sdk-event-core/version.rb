363f437c7ec762eea74b86c0e855ec7db805184e
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2019-08-10 12:42:03 -0500

    Preparing release of version 0.1.2

--- gems/icalia-sdk-event-core/lib/icalia-sdk-event-core/version.rb
+++ gems/icalia-sdk-event-core/lib/icalia-sdk-event-core/version.rb
@@ -2,6 +2,6 @@
 
 module Icalia
   module Event
-    CORE_VERSION = '0.1.1'
+    CORE_VERSION = '0.1.2'
   end
 end
--- gems/icalia-sdk/lib/icalia-sdk/version.rb
+++ gems/icalia-sdk/lib/icalia-sdk/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Icalia
-  VERSION = '0.1.1'
+  VERSION = '0.1.2'
 end
