a0ec959a7b608021da9267fce3c4d3cd430b3b80
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2019-08-10 19:25:37 -0500

    Fixed libraries depending on event-core not loading it

--- gems/icalia-sdk-event-notification/lib/icalia-sdk-event-notification.rb
+++ gems/icalia-sdk-event-notification/lib/icalia-sdk-event-notification.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'icalia-sdk-event-core'
+
 require 'active_support'
 require 'active_support/core_ext/object/blank'
 
--- gems/icalia-sdk-event-webhook/lib/icalia-sdk-event-webhook.rb
+++ gems/icalia-sdk-event-webhook/lib/icalia-sdk-event-webhook.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
+require 'icalia-sdk-event-core'
 require 'icalia-sdk-event-webhook/version'
