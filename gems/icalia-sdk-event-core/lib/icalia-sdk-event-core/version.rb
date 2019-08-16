55c1afe95dd244f6d790d9f4eea785249afd8421
Roberto Quintanilla <1915931+vovimayhem@users.noreply.github.com>
2019-08-16 12:58:30 -0500

    Prepare release of version 0.1.7 (#11)

--- gems/icalia-sdk-event-core/lib/icalia-sdk-event-core/version.rb
+++ gems/icalia-sdk-event-core/lib/icalia-sdk-event-core/version.rb
@@ -2,6 +2,6 @@
 
 module Icalia
   module Event
-    CORE_VERSION = '0.1.6'
+    CORE_VERSION = '0.1.7'
   end
 end
--- gems/icalia-sdk-event-notification/lib/icalia-sdk-event-notification/version.rb
+++ gems/icalia-sdk-event-notification/lib/icalia-sdk-event-notification/version.rb
@@ -2,6 +2,6 @@
 
 module Icalia
   module Event
-    NOTIFICATION_VERSION = '0.1.6'
+    NOTIFICATION_VERSION = '0.1.7'
   end
 end
