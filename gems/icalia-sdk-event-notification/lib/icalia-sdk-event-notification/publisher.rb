2c86df79b31e7f64aa899524812cf7fefc57d3ba
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2020-01-22 13:00:43 -0600

    Re-implemented publisher so it uses Google Cloud PubSub

--- gems/icalia-sdk-event-notification/lib/icalia-sdk-event-notification/publisher.rb
+++ gems/icalia-sdk-event-notification/lib/icalia-sdk-event-notification/publisher.rb
@@ -1,16 +1,21 @@
 # frozen_string_literal: true
 
-require 'aws-sdk-sns'
+require 'google/cloud/pubsub'
 
 require 'active_support'
 require 'active_support/logger'
 
 module Icalia::Event
+  module GRPCRailsLogger
+    def logger; Rails.logger; end
+  end
+
+  module GRPCStdoutLogger
+    LOGGER = ActiveSupport::Logger.new(STDOUT)
+    def logger; LOGGER; end
+  end
+
   class Publisher
-
-    class UnknownTopicError < StandardError
-    end
-
     class << self
       def instance
         @instance ||= new
@@ -19,41 +24,48 @@ module Icalia::Event
       delegate :publish, to: :instance
     end
 
-    attr_reader :sns_client, :logger
-
-    def initialize
-      set_sns_client
-      set_logger
-    end
-
-    def publish(topic_name, data, json_data: true)
-      raise UnknownTopicError, "Topic '#{topic_name}' does not exist" \
-        unless topic_exists?(topic_name)
-
-      topic = get_topic topic_name
-      logger.debug "Publishing to '#{topic_name}': #{data.inspect}"
-      topic.publish message: (json_data ? json_encode(data) : data)
-    end
+    attr_reader :client, :logger
 
     delegate :encode, to: ActiveSupport::JSON, prefix: :json
 
+    def initialize
+      initialize_pubsub_client
+      initialize_logger
+    end
+
+    def publish(topic_name, data, attributes = { json_data: true })
+      logger.debug "Publishing to '#{topic_name}': #{data.inspect}"
+      encoded_data = attributes[:json_data] ? json_encode(data) : data
+      get_or_create_topic(topic_name).publish(encoded_data, attributes)
+    end
+
     protected
 
-    def get_topic(topic_name)
-      sns_client.topics.detect do |topic|
-        topic.arn.split(':').last == topic_name
-      end
+    def get_or_create_topic(topic_name)
+      client.topic(topic_name) || client.new_topic(topic_name)
     end
 
-    def topic_exists?(topic_name)
-      get_topic(topic_name).present?
+    private
+
+    # See https://googleapis.dev/ruby/google-cloud-pubsub/latest/file.AUTHENTICATION.html#Environment_Variables
+    #
+    # The environment variables that google-cloud-pubsub checks for project ID are:
+    # 1. PUBSUB_PROJECT
+    # 2. GOOGLE_CLOUD_PROJECT
+    #
+    # The environment variables that google-cloud-pubsub checks for credentials
+    # are configured on Google::Cloud::PubSub::V1::Credentials:
+    #
+    # 1. PUBSUB_CREDENTIALS - Path to JSON file, or JSON contents
+    # 2. PUBSUB_KEYFILE - Path to JSON file, or JSON contents
+    # 3. GOOGLE_CLOUD_CREDENTIALS - Path to JSON file, or JSON contents
+    # 4. GOOGLE_CLOUD_KEYFILE - Path to JSON file, or JSON contents
+    # 5. GOOGLE_APPLICATION_CREDENTIALS - Path to JSON file
+    def initialize_pubsub_client
+      @client = Google::Cloud::Pubsub.new
     end
 
-    def set_sns_client
-      @sns_client = Aws::SNS::Resource.new Icalia::Event.sns_client_options
-    end
-
-    def set_logger
+    def initialize_logger
       return use_rails_logger if rails_logger_exist?
       use_default_logger
     end
@@ -64,10 +76,12 @@ module Icalia::Event
 
     def use_rails_logger
       @logger = Rails.logger
+      GRPCRailsLogger.send :extend, GRPCRailsLogger
     end
 
     def use_default_logger
       @logger = ActiveSupport::Logger.new(STDOUT)
+      GRPCStdoutLogger.send :extend, GRPCStdoutLogger
     end
   end
 end
