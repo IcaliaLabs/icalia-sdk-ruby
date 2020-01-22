5a8fbac405538f252280fb1ae2ac0554a20e28e8
Roberto Quintanilla <1915931+vovimayhem@users.noreply.github.com>
2020-01-22 17:12:29 -0600

    Migrate event notifications to google pubsub (#24)
    * Switched to debian-based Dockerfile to avoid problems with GRPC
    
    * Replaced aws gems with google-cloud-pubsub gem
    
    * Re-implemented publisher so it uses Google Cloud PubSub
    
    * Removed anything Shoryuken
    
    * Removed AWS options

--- gems/icalia-sdk-event-notification/lib/icalia-sdk-event-notification/shoryuken.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Icalia::Event
-  module Shoryuken
-    autoload :Worker, 'icalia-sdk-event-notification/shoryuken/worker'
-    autoload :WorkerRegistry, 'icalia-sdk-event-notification/shoryuken/worker_registry'
-    autoload :CLI, 'icalia-sdk-event-notification/shoryuken/cli'
-  end
-end
--- gems/icalia-sdk-event-notification/lib/icalia-sdk-event-notification/shoryuken/cli.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Icalia::Event::Shoryuken
-  module CLI
-    autoload :Base,   'icalia-sdk-event-notification/shoryuken/cli/base'
-    autoload :Runner, 'icalia-sdk-event-notification/shoryuken/cli/runner'
-    autoload :SQS,    'icalia-sdk-event-notification/shoryuken/cli/sqs'
-  end
-end
