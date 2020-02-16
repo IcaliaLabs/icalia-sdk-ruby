b89460433386f21d4b50e05fa9449250fa1d9b97
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2020-02-15 19:45:32 -0600

    Added Person#date_of_birth

--- gems/icalia-sdk-event-core/lib/icalia-sdk-event-core/models/person.rb
+++ gems/icalia-sdk-event-core/lib/icalia-sdk-event-core/models/person.rb
@@ -4,7 +4,8 @@ module Icalia
   class Person < ModelBase
     include ResourceTimestamps
     attr_reader :full_name, :given_name, :family_name,
-                :gender_type, :custom_gender
+                :gender_type, :custom_gender,
+                :date_of_birth
 
     alias name full_name
 
