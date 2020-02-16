310edb95660a07b3212c2262c33757001b01a722
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2020-02-15 19:43:20 -0600

    Added Person#gender_type and Person#custom_gender

--- gems/icalia-sdk-event-core/lib/icalia-sdk-event-core/models/person.rb
+++ gems/icalia-sdk-event-core/lib/icalia-sdk-event-core/models/person.rb
@@ -3,7 +3,8 @@
 module Icalia
   class Person < ModelBase
     include ResourceTimestamps
-    attr_reader :full_name, :given_name, :family_name
+    attr_reader :full_name, :given_name, :family_name,
+                :gender_type, :custom_gender
 
     alias name full_name
 
