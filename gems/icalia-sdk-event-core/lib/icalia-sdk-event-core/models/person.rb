2be9f442cbedf5b256e38d1ad4f29f447356d58b
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2020-02-15 19:42:24 -0600

    Changed Person#name for Person#full_name, and aliased it as #name

--- gems/icalia-sdk-event-core/lib/icalia-sdk-event-core/models/person.rb
+++ gems/icalia-sdk-event-core/lib/icalia-sdk-event-core/models/person.rb
@@ -3,7 +3,9 @@
 module Icalia
   class Person < ModelBase
     include ResourceTimestamps
-    attr_reader :name
+    attr_reader :full_name, :given_name, :family_name
+
+    alias name full_name
 
     has_many :email_accounts, :cloud_identities
   end
