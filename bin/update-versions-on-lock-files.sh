fbd50566cc357f269e2ed0a62c7b4420b408d341
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2020-03-07 23:49:46 -0600

    Fixed update lock files script

--- bin/update-versions-on-lock-files.sh
+++ bin/update-versions-on-lock-files.sh
@@ -5,6 +5,6 @@ set -e
 
 docker-compose run --rm event_notification bundle
 docker-compose run --rm event_webhook bundle
-docker-compose run --rm event_core bundle
+docker-compose run --rm core bundle
 docker-compose run --rm event_meta bundle
 docker-compose run --rm sdk_meta bundle
